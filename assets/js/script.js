73d1638f314188ba9acd0878235d863659946c88
Deividhsn <deividhsn@gmail.com>
2025-04-28 16:26:46 -0300

    Att-ajustes-finais

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,35 @@ function dorOpen(){
     // alert("teste");
     
     var element = document.getElementById("initial-page");
-    element.classList.add("teste");
+    setTimeout(() => {
+        element.classList.add("teste");
+        
+    }, 300);
+    setTimeout(() => {
+        element.classList.add("none");
+        
+    }, 1000);
     
     
 
 }
+
+function poemShow(){
+    // alert("teste");
+    
+    
+    var element = document.getElementById("btn-seg");
+        setTimeout(() => {
+            element.classList.add("teste");
+            
+        }, 300);
+        setTimeout(() => {
+            element.classList.add("none");
+            
+        }, 1000);
+        
+        
+   
+
+}
+
